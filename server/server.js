e27533a2ac3f8f3baf15242e0f79f8d65412b64e
Hannah Davantes <hannahdavantes@yahoo.com>
2025-11-20 22:50:33 -0700

    Added 404 and 500 error middlewares

--- server/server.js
+++ server/server.js
@@ -15,12 +15,26 @@ app.use(express.json());
 
 //Morgan - HTTP request logger
 import morgan from "morgan";
+import { StatusCodes } from "http-status-codes";
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
 //Routes
 app.use("/api/v1/events", eventRouter);
+
+//404 NOT FOUND error middleware
+app.use((req, res) => {
+  res.status(StatusCodes.NOT_FOUND).json({ error: "Invalid route" });
+});
+
+//500 SERVER ERROR error middleware
+app.use((err, req, res, next) => {
+  console.error(err);
+  res
+    .status(StatusCodes.INTERNAL_SERVER_ERROR)
+    .json({ error: "Something went wrong" });
+});
 
 const port = process.env.PORT || 5100;
 try {
